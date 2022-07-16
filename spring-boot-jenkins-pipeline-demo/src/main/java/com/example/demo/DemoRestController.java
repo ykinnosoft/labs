9e2784ae5bb66d258691f814303768d25ba2bb60
Shravan Keerthipati <shravan@ykinnosoft.com>
2022-07-16 18:59:36 +0530

    initial commits

---
+++ spring-boot-jenkins-pipeline-demo/src/main/java/com/example/demo/DemoRestController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+	
+	@GetMapping(path = "/greeting")
+	public String greeting(@RequestParam(required = false) String name) {
+		
+		if( null != name && !name.isEmpty())
+			return "Namaskar " + name;
+		
+		else
+			return "Namaskar..";
+	}
+
+}
